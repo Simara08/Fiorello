de9e2adf12370bc5c34f491c4d246de8e172a7d9
Simara08 <7knywpc@code.edu.az>
2022-05-22 14:08:31 +0400

    allapages

---
+++ template/assests/JS/main.js
@@ -0,0 +1,10 @@
+let Clickicon=document.getElementById(".search-icon");
+let Searchinput=document.getElementById("input");
+Clickicon.onclick=function Showinput() {
+    if (Searchinput.style.display=="none") {
+        Searchinput.style.display=="block";
+    }
+    else{
+        Searchinput.style.display=="none" 
+    }
+}
